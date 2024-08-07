501b1cb62963b0fe1988dffd04f693481d2cb8f7
Jed Allado <alladoJed@gmail.com>
2024-08-08 05:48:52 +0800

    First commit. Setup Angular.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'my-profile',
+        component: MyProfileComponent
+    }
+];
